01562304b0f800156d8813bb63aa4af8ada019cd
Felix Odhiambo <felixngwono@gmail.com>
2026-03-03 11:09:54 +0300

    StoryTeller

--- FelloMarley/settings.py
+++ FelloMarley/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -75,14 +76,9 @@ WSGI_APPLICATION = 'FelloMarley.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Jakamolo',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',  
-        'PORT': '3306',   
-    }
+    'default': dj_database_url.config(
+        default='mysql://root:@localhost:3306/Jakamolo'
+    )
 }
 
 
